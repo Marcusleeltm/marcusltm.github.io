a94ffcbe2fcb66ce81258266453944cb90505027
Marcusleeltm <marcusleeltm95@gmail.com>
2018-04-12 16:24:57 +0800

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_host = "localhost";
-$db_username = "root";
-$db_pass = "";
+$db_host = "tp038611ddac-mysqldbserver.mysql.database.azure.com";
+$db_username = "tp038611ddac@tp038611ddac-mysqldbserver";
+$db_pass = "Abc123456";
 $db_name = "ddac";
 
 $dbc = @mysqli_connect("$db_host", "$db_username", "$db_pass", $db_name) or die ("Could not connect to MySQL");
